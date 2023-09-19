29d0e052c0861c4deb72c1f2d4f54fb227d07c78
sdk250 <520sdk250@gmail.com>
2023-09-19 19:14:58 +0800

    Only for CN variant.

--- libinit/init_hiphic.cpp
+++ libinit/init_hiphic.cpp
@@ -16,26 +16,11 @@ void property_override(char const prop[], char const value[]) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void load_global() {
-    property_override("ro.build.fingerprint", "motorola/hiphi_g/hiphi:12/S3SH32.12-41-4/51104-0fc314:user/release-keys");
-    property_override("ro.product.brand", "motorola");
-    property_override("ro.product.device", "hiphi");
-    property_override("ro.product.model", "motorola edge 30 pro");
-}
 
-void load_cn() {
-    property_override("ro.build.fingerprint", "motorola/hiphic_cn/hiphic:12/S3SC32.7-42-8/c02b71-86451:user/release-keys");
+/* Only for CN variant */
+void vendor_load_properties() {
+    property_override("ro.build.fingerprint", "motorola/hiphic_cn/hiphic:12/T1SC33.52-8-6/63d55-35c24:user/release-keys");
     property_override("ro.product.brand", "motorola");
     property_override("ro.product.device", "hiphic");
     property_override("ro.product.model", "XT2201-2");
-}
-
-
-void vendor_load_properties() {
-    std::string region = android::base::GetProperty("ro.boot.hardware.sku", "");
-
-    if (region.find("XT2201-2") != std::string::npos)
-        load_cn();
-    else
-        load_global();
 }
